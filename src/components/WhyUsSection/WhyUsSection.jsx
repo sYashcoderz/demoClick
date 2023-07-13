eafa61371635cbacdadf53078b0077048d900fda
sYashcoderz <syash4505@gmail.com>
2023-07-13 18:12:32 +0530

    fix css

--- src/components/WhyUsSection/WhyUsSection.jsx
+++ src/components/WhyUsSection/WhyUsSection.jsx
@@ -8,7 +8,7 @@ export const WhyUsSection = () => {
     <Row>
       <Col span={24} className={classes.mainSection} style={{padding : '25px'}}>
         <Col span={24} style={{ display:'flex'}} className={classes.mainIntro}>
-          <Col span={24} style={{marginTop:'120px'}}>
+          <Col span={24} style={{marginTop:'110px'}}>
           <h1 style={{color: '#001B5D', fontSize: '2rem', fontFamily: 'sans-serif'}} > Why The Industry Chooses Clickdee?</h1>
           <p style={{fontSize: '.9375rem'}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin pretium nibh, eu placerat diam rutrum id. Duis ut congue nulla.
@@ -17,7 +17,7 @@ export const WhyUsSection = () => {
             Donec nisi neque, mattis vitae justo nec, luctus faucibus purus.
           </p>
           </Col>
-          <Col style={{ display:'flex', paddingLeft:'280px'}}>
+          <Col style={{ display:'flex', paddingLeft:'175px'}}>
             {[1, 2].map((each, _idx) => {
               return (
                 <div key={_idx}>
